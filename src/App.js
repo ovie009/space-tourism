7f8a2bebfbd69959d12ce1111ad9a6032ae91ff2
ovie009 <ovieiffieu2much@gmail.com>
2022-06-05 16:09:55 +0100

    navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from "./Navbar.js";
 
 function App() {
   return (
-    <body className="App">
-      
-    </body>
+    <div className="App">
+      <Navbar></Navbar>
+    </div>
   );
 }
 
---
+++ src/Navbar.js
@@ -0,0 +1,18 @@
+import "./Navbar.css";
+import logo from "./starter-code/assets/shared/logo.svg";
+import hamburger from "./starter-code/assets/shared/icon-hamburger.svg";
+
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <div className="container">
+                <img src={ logo } alt="space tourism logo" className="logo-icon"/>
+                <button type="button" id="toggle-list">
+                    <img src={hamburger} alt="Hamburger Icon" />
+                </button>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
